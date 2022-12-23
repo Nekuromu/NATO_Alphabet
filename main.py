4d15d0db67850d1071c29608544203c552668467
Nekuromu <30809120+Nekuromu@users.noreply.github.com>
2022-12-23 17:02:21 -0600

    error handling, tweaks to fix some errors, and looping for multiple uses added

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 import pandas
 
-pa_dict = {row.letter: row.code for index, row in pandas.read_csv("nato_phonetic_alphabet.csv").iterrows()}
+try:
+    pa_dict = {row.letter: row.code for index, row in pandas.read_csv("nato_phonetic_alphabet.csv").iterrows()}
+except FileNotFoundError:
+    print("Error: Could not find the file 'nato_phonetic_alphabet.csv'.")
+except Exception as e:
+    print(f"An error occurred while reading the file: {e}")
+    exit()
 
-user_input = input("Enter a word: ").split()
 
-result = [pa_dict[letter.upper()] for word in user_input for letter in word]
+def generate_phonetic():
+    user_input = input("Enter a word: ").split()
 
-print(" ".join(result))
+    try:
+        result = (" ".join([pa_dict[letter.upper()] for word in user_input for letter in word]))
+    except KeyError:
+        print(f"Error: numbers & symbols are not recognized letters in the NATO phonetic alphabet.")
+        result = None
+        generate_phonetic()
+
+    print(result)
+    generate_phonetic()
+
+
+generate_phonetic()
